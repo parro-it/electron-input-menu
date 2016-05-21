3b12958d084e3ad5005d372a96e0d4915332ee33
Andrea Parodi <andrea@parro.it>
2016-05-21 17:03:59 +0200

    removed eslint, adopt xo

--- index.js
+++ index.js
@@ -9,105 +9,103 @@ const DOM_VK_X = 0x58; // (88) "X" key.
 const DOM_VK_Z = 0x5A; // (90) "Z" key.
 
 const menuTemplate = [{
-  label: 'Undo',
-  role: 'undo',
+	label: 'Undo',
+	role: 'undo'
 }, {
-  label: 'Redo',
-  role: 'redo',
+	label: 'Redo',
+	role: 'redo'
 }, {
-  type: 'separator',
+	type: 'separator'
 }, {
-  label: 'Cut',
-  role: 'cut',
+	label: 'Cut',
+	role: 'cut'
 }, {
-  label: 'Copy',
-  role: 'copy',
+	label: 'Copy',
+	role: 'copy'
 }, {
-  label: 'Paste',
-  role: 'paste',
+	label: 'Paste',
+	role: 'paste'
 }, {
-  type: 'separator',
+	type: 'separator'
 }, {
-  label: 'Select all',
-  role: 'selectall',
+	label: 'Select all',
+	role: 'selectall'
 }];
 
 function action(name, evt) {
-  const win = electron.remote.getCurrentWindow();
-  win.webContents[name]();
-  evt.preventDefault();
-  return false;
+	const win = electron.remote.getCurrentWindow();
+	win.webContents[name]();
+	evt.preventDefault();
+	return false;
 }
 
-
-
 function isEditable(node) {
-  return node.matches('input, textarea, [contenteditable]');
+	return node.matches('input, textarea, [contenteditable]');
 }
 
 function handleInputShortcuts(evt) {
-    const c = evt.keyCode;
-    const ctrlDown = evt.ctrlKey || evt.metaKey; // OSX support
-    const altDown = evt.altKey;
-    const shiftDown = evt.shiftKey;
+	const c = evt.keyCode;
+	const ctrlDown = evt.ctrlKey || evt.metaKey; // OSX support
+	const altDown = evt.altKey;
+	const shiftDown = evt.shiftKey;
 
-    if (altDown) {
-      return true;
-    }
+	if (altDown) {
+		return true;
+	}
 
-     if (!isEditable(evt.target)) {
-      return true;
-    }
+	if (!isEditable(evt.target)) {
+		return true;
+	}
 
-    if (ctrlDown && !shiftDown && c === DOM_VK_C) {
-      return action('copy', evt);
-    }
+	if (ctrlDown && !shiftDown && c === DOM_VK_C) {
+		return action('copy', evt);
+	}
 
-    if (ctrlDown && !shiftDown && c === DOM_VK_V) {
-      return action('paste', evt);
-    }
+	if (ctrlDown && !shiftDown && c === DOM_VK_V) {
+		return action('paste', evt);
+	}
 
-    if (ctrlDown && !shiftDown && c === DOM_VK_X) {
-      return action('cut', evt);
-    }
+	if (ctrlDown && !shiftDown && c === DOM_VK_X) {
+		return action('cut', evt);
+	}
 
-    if (ctrlDown && !shiftDown && c === DOM_VK_A) {
-      return action('selectAll', evt);
-    }
+	if (ctrlDown && !shiftDown && c === DOM_VK_A) {
+		return action('selectAll', evt);
+	}
 
-    if (ctrlDown && !shiftDown && c === DOM_VK_Z) {
-      return action('undo', evt);
-    }
+	if (ctrlDown && !shiftDown && c === DOM_VK_Z) {
+		return action('undo', evt);
+	}
 
-    if (ctrlDown && shiftDown && c === DOM_VK_Z) {
-      return action('redo', evt);
-    }
+	if (ctrlDown && shiftDown && c === DOM_VK_Z) {
+		return action('redo', evt);
+	}
 
-    return true;
+	return true;
 }
 
 function registerShortcuts() {
-  if (document.body) {
-    document.body.addEventListener('onkeydown', handleInputShortcuts);
-  } else {
-    document.addEventListener('DOMContentLoaded', () => {
-      document.body.addEventListener('onkeydown', handleInputShortcuts);
-    });
-  }
+	if (document.body) {
+		document.body.addEventListener('onkeydown', handleInputShortcuts);
+	} else {
+		document.addEventListener('DOMContentLoaded', () => {
+			document.body.addEventListener('onkeydown', handleInputShortcuts);
+		});
+	}
 }
 
 function inputMenu(ctx, next) {
-  let node = ctx.elm;
+	let node = ctx.elm;
 
-  while (node) {
-    if (isEditable(node)) {
-      [].push.apply(ctx.menu, menuTemplate);
-      break;
-    }
-    node = node.parentElement;
-  }
-  next();
-};
+	while (node) {
+		if (isEditable(node)) {
+			[].push.apply(ctx.menu, menuTemplate);
+			break;
+		}
+		node = node.parentElement;
+	}
+	next();
+}
 
 inputMenu.registerShortcuts = registerShortcuts;
 
--- test.js
+++ test.js
@@ -2,12 +2,12 @@
 const electron = require('electron');
 
 electron.app.on('ready', () => {
-  const win = new electron.BrowserWindow({
-    show: true
-  });
-  const empty = electron.Menu.buildFromTemplate([]);
-  win.setMenu(empty);
-  electron.Menu.setApplicationMenu(empty);
-  win.loadURL('file://' + __dirname + '/example.html');
-  win.webContents.openDevTools();
+	const win = new electron.BrowserWindow({
+		show: true
+	});
+	const empty = electron.Menu.buildFromTemplate([]);
+	win.setMenu(empty);
+	electron.Menu.setApplicationMenu(empty);
+	win.loadURL(`file://${__dirname}/example.html`);
+	win.webContents.openDevTools();
 });
